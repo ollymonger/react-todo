7c4125bf416fab20d0ab04c2b57061df83ba13c1
Oliver Monger <omonger@neworbit.co.uk>
2019-06-19 16:31:00 +0100

    Add, remove and complete tasks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useState, Fragment } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import iTodo from './models/iTodo'
 
@@ -14,29 +13,25 @@ export default function App() {
     addTodo(value);
     console.log(todos);
   }
+
   const addTodo = (text: string): void => {
     const newTodos: iTodo[] = [...todos, { text, complete: false }];
     setTodos(newTodos);
   };
 
   const completeTodo = (index: number): void => {
-    const newTodos: iTodo[] = todos;
+    const newTodos: iTodo[] = [ ...todos ];
     newTodos[index].complete = !newTodos[index].complete;
     setTodos(newTodos);
   }
 
-  const updateTodo = (index: number): void => {
-    const newTodo: iTodo[] = todos;
-    if(newTodo[index].complete === true){
-      console.log("COMPLETE. NOW COMPLETE.")
-    }
-  }
-
   const removeTodo = (index: number): void => {
-    const newTodos: iTodo[] = todos;
+    const newTodos: iTodo[] = [...todos];
     newTodos.splice(index, 1);
     setTodos(newTodos);
   }
+ 
+
   return (
     <div className="App">
       <h1>Todo List</h1>
@@ -50,16 +45,15 @@ export default function App() {
         <button type='submit'>Add Todo</button>
       </form>
       <section>
-        {todos.map((todo: iTodo, index:number) => 
-          <div key={index} style={{ display: 'relative', textAlign:'center' }}>
-              <div style={{textDecoration:todo.complete ? 'line-through' : ''}}>
-                {todo.text} <button onClick={()=> completeTodo(index)}>{todo.complete ? 'Incomplete' : 'Complete'}</button>
-                <button onClick={()=> removeTodo(index)}>X</button>
-              </div>
+        {todos.map((todo: iTodo, index: number) => (
+          <div key={index}style={{ textDecoration: todo.complete ? 'line-through' : '' }}>
+              {todo.text}
+              <button onClick={() => completeTodo(index)}>{todo.complete ? 'Incomplete': 'Complete'}</button>
+              <button onClick={() => removeTodo(index)}>X</button>
           </div>
-        )}
+        ))}
       </section>
-      
+
     </div>
   );
 }
--- src/models/iTodo.ts
+++ src/models/iTodo.ts
@@ -1,4 +1,4 @@
 export default interface iTodo {
     text:string,
-    complete?: boolean
+    complete: boolean
 }
