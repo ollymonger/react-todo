ca3f70c6c2baa0e7b27c920aa02b2c9a8e0452ff
Olly Monger <olly@yllo.co.uk>
2019-06-25 16:13:12 +0100

    MenuBar is located in another file and imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { TodoModel } from './models/TodoModel'
 import { Todo } from "./components/todo";
-import { Button, TextField, Container, AppBar, Toolbar, IconButton, Typography, InputBase } from "@material-ui/core"
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
+import { Button, TextField, Container } from "@material-ui/core"
+import { MenuBar } from './components/MenuBar'
+
+import 'first-input-delay';
+import "firebase/performance";
 import './blankcss.css';
 
 type FormElem = React.FormEvent<HTMLFormElement>
+
+
 
 export interface taskProps {
   taskList: TodoModel;
@@ -25,7 +29,7 @@ const App: React.FunctionComponent = props => {
       return;
     }
 
-    const parsed: ({ title: string,dueMath:string, text: string, complete: boolean, due: string, created: string })[] = JSON.parse(loaded);
+    const parsed: ({ title: string, dueMath: string, text: string, complete: boolean, due: string, created: string })[] = JSON.parse(loaded);
 
     const datesTransformed: TodoModel[] = parsed.map(value => { //loads the data
       return {
@@ -35,7 +39,9 @@ const App: React.FunctionComponent = props => {
         complete: value.complete,
         due: new Date(value.due),
         created: new Date(value.created),
+        
       };
+      
     });
 
     setTodos(datesTransformed);
@@ -43,34 +49,32 @@ const App: React.FunctionComponent = props => {
 
   useEffect(() => { // runs whenever todo changes 
     document.title = `TaskList | You have ${todos.length} tasks`;
-
+    
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
-  const handleSubmit = (e: FormElem): void => {
+  const handleSubmit = (e: FormElem): void => { //handleSubmit
     e.preventDefault();
     setValue("");
     setTitle("");
-    addTodo(title, dueMath, value, '');
+    addTodo(title, dueMath, value);
     console.log(value);
   }
 
-  const addTodo = (title: string, dueMath:string, text: string, index: any): void => { //called when clicks
+  const addTodo = (title: string, dueMath: string, text: string): void => { //called when clicks
     const created = new Date();
 
     const due = new Date();
     const dueM = Number(dueMath);
     due.setDate(created.getDate() + dueM);
 
-    const newTodos: TodoModel[] = [...todos, { title,dueMath, text, due, complete: false, created }];
-
+    const newTodos: TodoModel[] = [...todos, { title, dueMath, text, due, complete: false, created }];
     setTodos(newTodos);
   };
 
   const completeTodo = (index: number): void => {
     const newTodos: TodoModel[] = [...todos];
     newTodos[index].complete = !newTodos[index].complete;
-
     setTodos(newTodos);
   }
 
@@ -82,31 +86,7 @@ const App: React.FunctionComponent = props => {
 
   return (
     <div>
-      <div>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="Open drawer"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" noWrap>
-              Material-UI
-          </Typography>
-            <div >
-              <div >
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search…"
-                inputProps={{ 'aria-label': 'Search' }}
-              />
-            </div>
-          </Toolbar>
-        </AppBar>
-      </div>
+      <MenuBar />
       <Container maxWidth="sm">
         <link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow|Righteous|Roboto|Open+Sans+Condensed|Material+Icons&display=swap" rel="stylesheet" />
         <h1 className="headericon">vertical_split</h1><h1>TaskList</h1>
@@ -133,26 +113,28 @@ const App: React.FunctionComponent = props => {
 
           />
           <TextField
-          className="daysText"
+            className="daysText"
             required
             id="standard-required"
             label="Due In(Days)"
             helperText="Insert your due in days here."
-            type='text'
+            type='number'
             value={dueMath}
             onChange={e => setDueMath(e.target.value)}
 
           />
-
           <Button type='submit' variant="outlined" color="secondary">Add Task</Button>
         </form>
         <section>
           {todos.map((todo: TodoModel, index: number) => (
-            <Todo todo={todo} key={index} onComplete={() => completeTodo(index)} onRemove={() => removeTodo(index)} />
+            
+            <Todo todo={todo} key={index}  onComplete={() => completeTodo(index)} onRemove={() => removeTodo(index)} />
+            
           ))}
+                    
         </section>
       </Container>
-    </div>
+    </div >
   );
 };
 
--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,24 +1,22 @@
 import React from "react";
 import { TodoModel } from "../models/TodoModel";
 
-import { Button, TextField, Container, Grid } from "@material-ui/core"
+import { Button } from "@material-ui/core"
 
 interface TodoProps {
     todo: TodoModel;
-
     onComplete: () => void;
     onRemove: () => void;
 }
 
 const Todo: React.FunctionComponent<TodoProps> = ({ todo, onComplete, onRemove }) => {
     return (
-        <div style={{ textDecoration: todo.complete ? 'line-through' : '' }}>
+            <div style={{ textDecoration: todo.complete ? 'line-through' : '' }}>
+                <p>{todo.title}</p><p>{todo.text}</p> Due: <p>{todo.due.toDateString()}</p>
+                <Button color="primary" onClick={onComplete}>{todo.complete ? 'Incomplete' : 'Complete'}</Button>
+                <Button color="secondary" onClick={onRemove}>Delete</Button>
 
-            <p>{todo.title}</p><p>{todo.text}</p> Due: <p>{todo.due.toDateString()}</p>
-            <Button color="primary" onClick={onComplete}>{todo.complete ? 'Incomplete' : 'Complete'}</Button>
-            <Button color="secondary" onClick={onRemove}>Delete</Button>
-
-        </div>
+            </div>
     );
 };
 
