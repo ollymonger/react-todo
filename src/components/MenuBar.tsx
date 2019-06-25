87d0f9c1835e76abd65353b007f299abf9daaef0
Olly Monger <olly@yllo.co.uk>
2019-06-25 16:58:53 +0100

    Added a menu button on menubar

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Toolbar, IconButton, Typography, InputBase } from "@material-ui/core"
-import {AppBar} from "@material-ui/core"
+import { Toolbar, Typography, InputBase} from "@material-ui/core"
+import { AppBar, Button } from "@material-ui/core"
 import MenuIcon from '@material-ui/icons/Menu';
 import { fade, makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
@@ -10,104 +10,96 @@ import MenuItem from '@material-ui/core/MenuItem';
 
 
 const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
     },
-    menuButton: {
-      marginRight: theme.spacing(2),
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
     },
-    title: {
-      flexGrow: 1,
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'block',
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
       },
     },
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-      },
-    },
-    searchIcon: {
-      width: theme.spacing(7),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    inputRoot: {
-      color: 'inherit',
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 7),
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: 120,
-        '&:focus': {
-          width: 200,
-        },
-      },
-    },
-  }));
+  },
+}));
 
 const MenuBar: React.FunctionComponent = props => {
 
-    const classes = useStyles(1);
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <PopupState variant="popover" popupId="demo-popup-menu">
-                    {popupState => (
-                        <React.Fragment>
-                            <IconButton
-                                edge="start"
-                                className={classes.menuButton}
-                                color="inherit"
-                                aria-label="Open drawer"
-                                variant="contained" {...bindTrigger(popupState)}
-                            >
+  const classes = useStyles(1);
 
-                                <MenuIcon />
-                                <Menu {...bindMenu(popupState)}>
-                                    <MenuItem onClick={popupState.close}>TaskLists</MenuItem>
-                                    <MenuItem onClick={popupState.close}>Test</MenuItem>
-                                </Menu>
-
-                            </IconButton>
-                        </React.Fragment>
-                    )}
-                </PopupState>
-                <Typography className={classes.title} variant="h6" noWrap>
-                    TaskLists
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <PopupState variant="popover" popupId="demo-popup-menu">
+          {popupState => (
+            <React.Fragment>
+              <Button variant="contained" {...bindTrigger(popupState)}>
+              <MenuIcon />
+          </Button>
+              <Menu {...bindMenu(popupState)}>
+                <MenuItem onClick={popupState.close}>TaskLists</MenuItem>
+                <MenuItem onClick={popupState.close}>Coming</MenuItem>
+              </Menu>
+            </React.Fragment>
+          )}
+        </PopupState>
+        <Typography className={classes.title} variant="h6" noWrap>| TaskLists
           </Typography>
-                <div className={classes.search}>
-                    <div className={classes.searchIcon}>
-                        <SearchIcon />
-                    </div>
-                    <InputBase
-                        placeholder="Search…"
-                        classes={{
-                            root: classes.inputRoot,
-                            input: classes.inputInput,
-                        }}
-                        inputProps={{ 'aria-label': 'Search' }}
-                    />
-                </div>
-            </Toolbar>
-        </AppBar>
-    )
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'Search' }}
+          />
+        </div>
+      </Toolbar>
+    </AppBar>
+  )
 };
 
-export {MenuBar};
+export { MenuBar };
